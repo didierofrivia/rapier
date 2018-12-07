4a5d0eada8cd746ec2f9e24c68568d95a8e3c8a3
dd di cesare <didi@posteo.net>
2018-12-07 10:20:40 +0100

    [navigation] some more meaningful routing and navigation

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Url
+import Url.Parser exposing ((</>), Parser, int, map, oneOf, s, string, top)
 
 
 
@@ -30,12 +31,25 @@ main =
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , route : Route
+    }
+
+
+type Route
+    = Dashboard
+    | Settings
+    | NotFound
+
+
+type alias Page msg =
+    { title : String
+    , body : List (Html msg)
     }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url, Cmd.none )
+    ( Model key url Dashboard, Cmd.none )
 
 
 
@@ -59,9 +73,22 @@ update msg model =
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            ( { model | url = url }
+            ( { model | route = fromUrl url }
             , Cmd.none
             )
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ map Dashboard top
+        , map Settings (s "settings")
+        ]
+
+
+fromUrl : Url.Url -> Route
+fromUrl url =
+    Maybe.withDefault NotFound (Url.Parser.parse routeParser url)
 
 
 
@@ -79,18 +106,71 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "App bare bones - changing url"
-    , body =
-        [ text "The current URL is: "
-        , b [] [ text (Url.toString model.url) ]
-        , ul []
-            [ viewLink "/dashboard"
-            , viewLink "/settings"
-            ]
-        ]
+    case model.route of
+        Dashboard ->
+            viewDashboardPage model
+
+        Settings ->
+            viewDashboardPage model
+
+        NotFound ->
+            viewNotFound model
+
+
+buildPage : String -> List (Html msg) -> Page msg
+buildPage title body =
+    { title = title
+    , body = body
     }
 
 
-viewLink : String -> Html msg
-viewLink path =
-    li [] [ a [ href path ] [ text path ] ]
+header : Html msg
+header =
+    div []
+        [ p [] [ text "Header" ]
+        , ul []
+            [ viewLink "/" "Dashboard"
+            , viewLink "/settings" "Settings"
+            , viewLink "/asdasd" "asdsad"
+            ]
+        ]
+
+
+footer : Html msg
+footer =
+    div []
+        [ p [] [ text "Footer" ]
+        ]
+
+
+template : Html msg -> List (Html msg)
+template content =
+    [ header
+    , content
+    , footer
+    ]
+
+
+viewDashboardPage : Model -> Page msg
+viewDashboardPage model =
+    buildPage "Dashboard Page"
+        (template
+            (div []
+                [ p [] [ text "Dashboard" ] ]
+            )
+        )
+
+
+viewNotFound : Model -> Page msg
+viewNotFound model =
+    buildPage "Not Found"
+        (template
+            (div []
+                [ p [] [ text "Not Found" ] ]
+            )
+        )
+
+
+viewLink : String -> String -> Html msg
+viewLink path humanName =
+    li [] [ a [ href path ] [ text humanName ] ]

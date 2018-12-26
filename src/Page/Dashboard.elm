a0fb45872b3e616c88d0d42ccaaf0a7265b2434e
dd di cesare <didi@posteo.net>
2018-12-26 17:25:24 +0700

    [styling] Main components and rough styling with Bulma
    * Also branding ;) o-_)=rapier>

--- src/Page/Dashboard.elm
+++ src/Page/Dashboard.elm
@@ -1,5 +1,12 @@
 module Page.Dashboard exposing (Model, Msg(..), view)
 
+import Bulma.Elements as Elements exposing (TitleSize(..), title)
+import Bulma.Layout exposing (container, hero, heroBody)
+import Bulma.Modifiers
+    exposing
+        ( Color(..)
+        , Size(..)
+        )
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -16,5 +23,24 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ button [ onClick Noop ] [ text "Le Dashboard page" ] ]
+    hero myHeroModifiers
+        []
+        [ heroBody []
+            [ container []
+                [ Elements.title H1 [] [ text "o-_)=rapier>" ]
+                , Elements.title H2
+                    []
+                    [ text "An "
+                    , a [ href "https://github.com/3scale/apicast" ] [ text "APIcast" ]
+                    , text " UI MGMT tool."
+                    ]
+                ]
+            ]
+        ]
+
+
+myHeroModifiers =
+    { bold = True
+    , size = Standard
+    , color = Light
+    }
--- src/Page/NotFound.elm
+++ src/Page/NotFound.elm
@@ -1,5 +1,12 @@
 module Page.NotFound exposing (Model, Msg(..), view)
 
+import Bulma.Elements as Elements exposing (TitleSize(..), title)
+import Bulma.Layout exposing (container, hero, heroBody)
+import Bulma.Modifiers
+    exposing
+        ( Color(..)
+        , Size(..)
+        )
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -16,5 +23,22 @@ type Msg
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ p [ onClick Noop ] [ text "Not Found" ] ]
+    hero myHeroModifiers
+        []
+        [ heroBody []
+            [ container []
+                [ Elements.title H1 [] [ text "Oops! You got yourself in a pickle!" ]
+                , Elements.title H2
+                    []
+                    [ a [ href "/" ] [ text "o-_)==> take me home <==(_-o" ]
+                    ]
+                ]
+            ]
+        ]
+
+
+myHeroModifiers =
+    { bold = True
+    , size = Standard
+    , color = Warning
+    }

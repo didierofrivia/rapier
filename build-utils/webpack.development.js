2dcbf2d6f84807462c7ed979a4b3a04c3d3f33a4
dd di cesare <didi@posteo.net>
2019-04-12 20:51:51 +0700

    [webpack] Webpack + goodies
    * Webpack config (front)
    * Handy scripts
    * Webpack dashboard <3

---
+++ build-utils/webpack.development.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const webpack = require('webpack');
+
+module.exports = () => ({
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          { loader: 'elm-hot-webpack-loader' },
+          {
+            loader: 'elm-webpack-loader',
+            options: {
+              cwd: path.join(__dirname, '../'),
+              debug: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              babelrc: true,
+              compact: false,
+              presets: [
+                [
+                  // Latest stable ECMAScript features
+                  require('@babel/preset-env').default,
+                  {
+                    // Do not transform modules to CJS
+                    modules: false
+                  }
+                ]
+              ],
+              cacheDirectory: true,
+              highlightCode: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|otf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?|(jpg|gif)$/,
+        loader: 'file-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+
+    new DashboardPlugin(),
+  ],
+
+  devServer: {
+    contentBase: './src',
+    historyApiFallback: true,
+    inline: true,
+    stats: 'errors-only',
+    hot: true,
+  }
+});
